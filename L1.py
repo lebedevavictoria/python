8604b1e377826d2a03d1658fe891e647677f4ec8
lebedevavictoria <123237338+lebedevavictoria@users.noreply.github.com>
2023-02-15 17:49:38 +0300

    Delete L1.py

--- L1.py
+++
@@ -1,6 +0,0 @@
-with open('new_file1.txt', 'a') as file:
-    while True:
-        user_answ = input('Напишите текст: ')
-        file.write(user_answ + '\n')
-        if not user_answ:
-            break
