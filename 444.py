174471bbcf2b344d3a9d113317a5f8fe3b39619e
lebedevavictoria <123237338+lebedevavictoria@users.noreply.github.com>
2023-02-15 20:03:52 +0300

    Delete 444.py

--- 444.py
+++
@@ -1,8 +0,0 @@
-text = input("Пожалуйста введите текст : ")
-T = text.split()
-for x, y in enumerate (T, start=1) :
-    if len(y) > 11 :
-        y = y[:10]
-        print(x, y)
-    else :
-        print (x,y)
